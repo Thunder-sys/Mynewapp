73f4129fcee9d3f93ca233dbe959f1b61eaf0e1c
I58gen <yashpal14700@gmail.com>
2024-01-02 10:35:18 +0530

    Add List only for Text

---
+++ app/src/main/java/com/example/mynewapp/Listwithetxtimg/Listimgwtxt.kt
@@ -0,0 +1,12 @@
+package com.example.mynewapp.Listwithetxtimg
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mynewapp.R
+
+class Listimgwtxt : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_listimgwtxt)
+    }
+}
